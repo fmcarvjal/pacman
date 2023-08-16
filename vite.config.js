f89614bee61eee9d337935fbe7051ba325eb644f
fabianc@uazuay.edu.ec <fabianc@uazuay.edu.ec>
2023-08-16 08:00:45 -0500

    inicial

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://fmcarvjal.github.io/hanhs/",
+  base: "https://fmcarvjal.github.io/pacman/",
 });
